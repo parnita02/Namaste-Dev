a5f316dc0ebeb1a200d3ef025072f2ff23a320df
parnita02 <parnitadwivedi02@gmail.com>
2024-07-16 19:40:36 +0530

    class component done

--- FoodOrderingApp/src/components/AboutUs.js
+++ FoodOrderingApp/src/components/AboutUs.js
@@ -1,10 +1,10 @@
-import User from "./User";
+// import User from "./User";
 import UserClass from "./UserClass";
 const AboutUs = () => {
   return (
     <div>
       <h1>this is the about component</h1>
-      <User name={"Parnita Dwivedi (functional)"} />
+      {/* <User name={"Parnita Dwivedi (functional)"} /> */}
       <UserClass name={"Parnita Dwivedi(class)"} />
     </div>
   );
--- FoodOrderingApp/src/components/UserClass.js
+++ FoodOrderingApp/src/components/UserClass.js
@@ -5,14 +5,29 @@ class UserClass extends React.Component {
     super(props);
     // console.log(props);
     this.state = {
+      userInfo: {
+        name: "abc",
+        location: "xyz",
+        avatar_url: "",
+      },
       count: 0,
     };
   }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/parnita02");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    // console.log(json);
+  }
   render() {
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div className="userAbout">
-        <h1>count: {this.state.count}</h1>
-        <button
+        {/* <h1>count: {this.state.count}</h1> */}
+        {/* <button
           onClick={() =>
             this.setState({
               count: this.state.count + 1,
@@ -20,10 +35,10 @@ class UserClass extends React.Component {
           }
         >
           click
-        </button>
-        <h2> Name: {this.props.name}</h2>
-        <h2>Location: Rewa, Madhya Pradesh</h2>
-        <h2>Contact: parnitadwivedi02@gmail.com</h2>
+        </button> */}
+        <img src={avatar_url} />
+        <h2> Name: {name}</h2>
+        <h2>Location: {location}</h2>
       </div>
     );
   }
